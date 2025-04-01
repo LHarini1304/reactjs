1ce1b07f75403d6519c2ffd8549aee708dbc2d4e
LHarini1304 <lharini1311@gmail.com>
2025-04-01 16:49:32 +0530

    version_01

---
+++ app.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { key: "child1" }, [
+    React.createElement("h1", { key: "h1-child1" }, "heading1"),
+    React.createElement("h2", { key: "h2-child1" }, "heading2"),
+  ]),
+  React.createElement("div", { key: "child2" }, [
+    React.createElement("h1", { key: "h1-child2" }, "heading3"),
+    React.createElement("h2", { key: "h2-child2" }, "heading4"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
