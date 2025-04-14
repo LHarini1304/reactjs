4b667e71676e6b71f8a8c01e9fccf32edc5a432d
LHarini1304 <lharini1311@gmail.com>
2025-04-14 16:22:07 +0530

    version_05 Hook-useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,71 @@
 import RestaurantCard from "./Restaurant";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  let [resData, setResData] = useState(resList); // State to manage restaurant data
-  return (
+  const [listOfRestaurant, setListOfRestaurant] = useState([]); // State to manage restaurant data
+  const [searchText, setSearchText] = useState(""); // State to manage search text
+  const [filteredResData, setFilteredResData] = useState([]); // State to manage filtered restaurant data
+  // whenever state variable updates, the react trigger reconciliation process (re-rendering) to reflect the changes in the UI.
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const jsonData = await data.json();
+    setListOfRestaurant(
+      //optional chaining operator (?.) is used to access deeply nested properties of an object without having to check if each reference in the chain is nullish (null or undefined).
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilteredResData(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    ); // Initialize filteredResData with the same data as resData
+  };
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value); // Update search text state
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              const filterRes = listOfRestaurant.filter(
+                (restaurant) =>
+                  restaurant.info.name
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase()) // Filter restaurant data based on search text
+              );
+              setFilteredResData(filterRes); // Update state with filtered data
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             // Filter restaurant data based on rating
             console.log("Top Rated Restaurant");
-            const filteredData = resData.filter(
-              (restaurant) => restaurant.info.avgRating > 4.2
+            const filteredData = listOfRestaurant.filter(
+              (restaurant) => restaurant.info.avgRating > 4.1
             );
-            setResData(filteredData); // Update state with filtered data
+            setFilteredResData(filteredData); // Update state with filtered data
           }}
         >
           Top Rated Restaurant
@@ -27,7 +77,7 @@ const Body = () => {
           /* Using key to identify each restaurant card. 
                  Reason: React uses keys to identify which items have changed, are added, or are removed, 
                  So that it can optimize the rendering process , it won't re-render the entire list, only the items that have changed. */
-          resData.map((restaurant) => (
+          filteredResData.map((restaurant) => (
             <RestaurantCard key={restaurant.info.id} resdata={restaurant} />
           ))
         }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [buttontype, setButtonType] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,17 @@ const Header = () => {
           <li>About</li>
           <li>Contact</li>
           <li>Cart</li>
+          <button
+            className="LoginButton"
+            onClick={() => {
+              buttontype === "Login"
+                ? setButtonType("Logout")
+                : setButtonType("Login");
+              console.log("Login button clicked", buttontype);
+            }}
+          >
+            {buttontype}
+          </button>
         </ul>
       </div>
     </div>
